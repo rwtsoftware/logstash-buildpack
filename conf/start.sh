14139e7ae0f64638a458c9f66d62cb2b4f3df1e9
Ryan Taylor <taylorry@us.ibm.com>
2016-04-13 12:40:32 -0400

    testing

--- conf/start.sh
+++ conf/start.sh
@@ -8,4 +8,6 @@ mkfifo $APP_ROOT/.bp/nginx/logs/error.log
 cat < $APP_ROOT/.bp/nginx/logs/access.log &
 (>&2 cat) < $APP_ROOT/.bp/nginx/logs/error.log &
 
-exec nginx -p $APP_ROOT/.bp/nginx -c $APP_ROOT/nginx.conf
+exec nginx -p $APP_ROOT/.bp/nginx -c $APP_ROOT/nginx.conf &
+exec logstash agent -f ./logstash.conf &
+wait
