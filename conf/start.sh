870614964d7a12f7df69bc364568725c7cb6d235
Ryan Taylor <taylorry@us.ibm.com>
2016-04-13 12:10:45 -0400

    testing

--- conf/start.sh
+++ conf/start.sh
@@ -2,10 +2,10 @@ export APP_ROOT=$HOME
 
 erb $APP_ROOT/nginx.conf.erb > $APP_ROOT/nginx.conf
 
-mkfifo $APP_ROOT/nginx/logs/access.log
-mkfifo $APP_ROOT/nginx/logs/error.log
+mkfifo $APP_ROOT/.bp/nginx/logs/access.log
+mkfifo $APP_ROOT/.bp/nginx/logs/error.log
 
-cat < $APP_ROOT/nginx/logs/access.log &
-(>&2 cat) < $APP_ROOT/nginx/logs/error.log &
+cat < $APP_ROOT/.bp/nginx/logs/access.log &
+(>&2 cat) < $APP_ROOT/.bp/nginx/logs/error.log &
 
 exec nginx -c $APP_ROOT/nginx.conf
