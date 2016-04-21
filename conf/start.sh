64f293ddac0b8b6e5a91dff9277a9696b30a2bd0
Ryan Taylor <taylorry@us.ibm.com>
2016-04-21 08:59:29 -0400

    Adding logging

--- conf/start.sh
+++ conf/start.sh
@@ -2,12 +2,21 @@ export APP_ROOT=$HOME
 
 erb $APP_ROOT/nginx.conf.erb > $APP_ROOT/nginx.conf
 
-mkfifo $APP_ROOT/.bp/nginx/logs/access.log
-mkfifo $APP_ROOT/.bp/nginx/logs/error.log
+mkfifo $APP_ROOT/../logs/nginx_access.log
+mkfifo $APP_ROOT/../logs/nginx_error.log
 
-cat < $APP_ROOT/.bp/nginx/logs/access.log &
-(>&2 cat) < $APP_ROOT/.bp/nginx/logs/error.log &
+cat < $APP_ROOT/../logs/nginx_access.log &
+(>&2 cat) < $APP_ROOT/../logs/nginx_error.log &
 
 exec nginx -p $APP_ROOT/.bp/nginx -c $APP_ROOT/nginx.conf &
+PID_NGINX=$!
+
+mkfifo $APP_ROOT/../logs/logstash.log
+
+cat < $APP_ROOT/../logs/logstash.log &
+
 exec logstash agent -f ./logstash.conf &
-wait
+PID_LOGSTASH=$!
+
+wait $PID_NGINX
+wait $PID_LOGSTASH
